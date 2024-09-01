2c221735d05fc29ca4107d7eea3665c9ed495b98
hoonseung <dltmdgns4029@gmail.com>
2024-09-01 20:48:32 +0800

    init : 프로젝트 시작
    gitginore, gradle 초기화

---
+++ src/main/java/com/onion/backend/CommunityOnionApplication.java
@@ -0,0 +1,13 @@
+package com.onion.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityOnionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityOnionApplication.class, args);
+    }
+
+}
