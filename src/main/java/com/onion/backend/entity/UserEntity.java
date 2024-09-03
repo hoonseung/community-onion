b7bc2b6afc2babc8c981fb5b55ffd3e84e0266c0
hoonseung <dltmdgns4029@gmail.com>
2024-09-03 21:49:04 +0800

    #user-domain : user entity 작성
    기본적인 user 테이블 엔티티를 작성하고 공통으로 사용될 베이스 엔티티에 jpa auditing 기능까지 적용함

---
+++ src/main/java/com/onion/backend/entity/UserEntity.java
@@ -0,0 +1,45 @@
+package com.onion.backend.entity;
+
+import com.onion.backend.entity.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "\"users\"")
+@Entity
+public class UserEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column()
+    private LocalDateTime lastLogin;
+
+
+    public static UserEntity of(String username, String password, String email) {
+        return new UserEntity(null, username, password, email, null);
+    }
+
+
+}
